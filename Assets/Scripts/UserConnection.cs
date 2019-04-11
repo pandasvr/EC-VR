ea8888265c88b5f6102e14ff20e3fda6049aafcb
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-04-11 15:17:41 +0200

    Add BDD config and c# script for connection

---
+++ Assets/Scripts/UserConnection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class UserConnection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField fieldName;
+    public InputField fieldPassword;
+
+    public void CallConnection()
+    {
+        StartCoroutine(Connection());
+    }
+
+    IEnumerator Connection()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", fieldName.text);
+        form.AddField("password", fieldPassword.text);
+        
+        Debug.Log("nameField :" + fieldName.text);
+        Debug.Log("passwordField :" + fieldPassword.text);
+
+        UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1/edsa-unitySQL/connection.php", form);
+        www.downloadHandler = new DownloadHandlerBuffer();
+        yield return www.SendWebRequest();
+ 
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            Debug.Log("Post request complete!" + " Response Code: " + www.responseCode);
+            string responseText = www.downloadHandler.text;
+            Debug.Log("Response Text:" + responseText);
+            
+            if (responseText == "false")
+            {
+                Debug.Log("wrong name or password");
+            }
+            if(responseText == "true")
+            {
+                Debug.Log("user connected");
+            }
+            
+        }
+    }
+}
---
+++ SQL/class.pdoUnity.inc.php
@@ -0,0 +1,48 @@
+<?php
+class PdoUnity
+{   		
+      	private static $server='mysql:host=localhost';
+      	private static $bdd='dbname=ecvr_db';
+      	private static $user='root';
+      	private static $pw='';
+		private static $myPdo;
+		private static $myPdoUnity = null;
+/**
+ * Constructeur privé, crée l'instance de PDO qui sera sollicitée
+ * pour toutes les méthodes de la classe
+ */				
+	private function __construct()
+	{
+    		PdoUnity::$myPdo = new PDO(PdoUnity::$server.';'.PdoUnity::$bdd, PdoUnity::$user, PdoUnity::$pw); 
+			PdoUnity::$myPdo->query("SET CHARACTER SET utf8");
+			PdoUnity::$myPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			PdoUnity::$myPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	}
+	public function _destruct()
+	{
+		PdoUnity::$myPdo = null;
+	}
+/**
+ * Fonction statique qui crée l'unique instance de la classe
+ */
+	public static function getPdoUnity()
+	{
+		if(PdoUnity::$myPdoUnity == null)
+		{
+			PdoUnity::$myPdoUnity= new PdoUnity();
+		}
+		return PdoUnity::$myPdoUnity;
+	}
+
+	
+/**
+ * Fonction qui vérifie le login et le password pour la connexion au site
+ */
+	public function userConnect($login,$password)
+	{
+		$req="SELECT * FROM users where nameUser = '".$login."' AND passwordUser = '".$password."'";
+		$resultat=PdoUnity::$myPdo->query($req)->fetchAll();
+		return $resultat;
+	}
+}
+?>
