85a066983d4658a0365a6022312037b4846bb769
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-04-29 11:49:26 +0200

    corrections page inscription
    éléments UI replacés
    corrections script connexion

--- Assets/Scripts/BDD/SignIn.cs
+++ Assets/Scripts/BDD/SignIn.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -12,6 +9,10 @@ public class SignIn : MonoBehaviour
     //champs noms et password récupérés depuis l'UI d'unity
     public InputField fieldName;
     public InputField fieldPassword;
+    
+    public GameObject LoginPanel;
+    public GameObject MainPanel;
+    public Text InfoText;
 
     private bool isValidPassword;
     private String userPassword;
@@ -27,21 +28,6 @@ public class SignIn : MonoBehaviour
     //Connexion de l'utilisateur
     IEnumerator ServerSend()
     {
-        CspParameters cspParams = new CspParameters();
-
-        // Les clés publique et privée
-        RSAParameters privateKeys;
-        RSAParameters publicKeys;
-
-        using (var rsa = new RSACryptoServiceProvider(cspParams))
-        {
-            // Génère la clé publique et la clé privée
-            privateKeys = rsa.ExportParameters(true);
-            publicKeys = rsa.ExportParameters(false);
-
-            rsa.Clear();
-        }
-        
         //Récupération des valeurs du formulaire
         userPassword = Hashing.HashPassword(fieldPassword.text); //on hache directement le mot de passe récupéré
         userName = fieldName.text;// on récupère directement le username
@@ -52,13 +38,16 @@ public class SignIn : MonoBehaviour
         Debug.Log("nameField :" + userName);
         Debug.Log("passwordField :" + userPassword);
 
+        
         //Envoie des données au serveur
         UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1/edsa-unitySQL/signin.php", form);
+        
         
         //Récupération du retour serveur
         www.downloadHandler = new DownloadHandlerBuffer();
         yield return www.SendWebRequest();
  
+        
         //Vérification du retour
         if(www.isNetworkError || www.isHttpError) {
             Debug.Log(www.error);
@@ -67,20 +56,45 @@ public class SignIn : MonoBehaviour
             Debug.Log("Post request complete!" + " Response Code: " + www.responseCode);
             string responseText = www.downloadHandler.text;
             Debug.Log("Response Text encrypt :" + responseText);
-            
-            decryptedPassword = Crypting.Decrypt(responseText);
-            Debug.Log("Response Text decrypt :" + decryptedPassword);
-            isValidPassword = Hashing.ValidatePassword(fieldPassword.text, decryptedPassword);
-            
-            if (isValidPassword)
+
+            if (!string.IsNullOrEmpty(responseText))
             {
-                Debug.Log("user connected");
+                decryptedPassword = Crypting.Decrypt(responseText);
+                Debug.Log("Response Text decrypt :" + decryptedPassword);
+                isValidPassword = Hashing.ValidatePassword(fieldPassword.text, decryptedPassword);
+
+                if (isValidPassword)
+                {
+                    Debug.Log("user connected");
+                    LoginPanel.SetActive(false);
+                    MainPanel.SetActive(true);
+                    fieldPassword.text=""; //on vide les champs pour que les identifiants ne soient pas "sauvegardés" par la page de connexion
+                    fieldName.text = "";
+                    
+                    if (InfoText.enabled)
+                    {
+                        InfoText.enabled = false;
+                        InfoText.gameObject.SetActive(false);
+                    }
+                }
+                else
+                {
+                    Debug.Log("wrong username or password");
+                    InfoText.text = "wrong username or password";
+                    InfoText.color = Color.red;
+                    InfoText.enabled = true;
+                    InfoText.gameObject.SetActive(true);
+                }
             }
             else
             {
-                Debug.Log("wrong name or password");
+                Debug.Log("wrong username or password");
+                InfoText.text = "wrong username or password";
+                InfoText.color = Color.red;
+                InfoText.enabled = true;
+                InfoText.gameObject.SetActive(true);
             }
-            
+
         }
     }
 }
--- SQL/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "coucou";
-?>
