971e99fffa60a6cfc8c20940e8995b42f59214d0
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-05-03 15:21:55 +0200

    Tri en dans assets et les scènes

---
+++ Assets/1_Scripts/ApplicationQuit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuit : MonoBehaviour
+{
+    public void DoQuitApp()
+    {
+        Debug.Log("L'utilisateur a quitté l'application.");
+        Application.Quit();
+    }
+}
---
+++ Assets/1_Scripts/Networking/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+namespace Networking
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
+        private void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManager.GetActiveScene().name);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(-1f,0f,-2f), Quaternion.identity, 0);
+            }
+
+        }
+
+        #region Photon Callbacks
+
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
+        {        
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+        
+        #region Private Methods
+
+
+
+        #endregion
+        
+        #region Photon Callbacks
+
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+        }
+
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+        }
+
+
+        #endregion
+    }
+    
+    
+    
+}
