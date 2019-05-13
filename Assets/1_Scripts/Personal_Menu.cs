b2db5f1ea706b25efe659e232d63a20895545f27
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-05-13 10:01:33 +0200

    Menu personnel fonctionel
    fonction d'appel ne fonctionne pas pour l'instant

---
+++ Assets/1_Scripts/Personal_Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   public class Personal_Menu: MonoBehaviour
+    {    
+        
+        public GameObject personnalMenuPanel;
+
+        
+        // Update is called once per frame
+        public void ActivePersonalMenu()
+        {
+            personnalMenuPanel.SetActive(true);
+            personnalMenuPanel.gameObject.SetActive(true);
+        }
+        
+        
+    }
