fb6555b083da6af0ee6e1c7269c7f975cc731e67
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-05-14 11:37:25 +0200

    bouton "vidéo" lance le videoplayer

---
+++ Assets/1_Scripts/InTheReunionRoom/MediaShare.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MediaShare : MonoBehaviour
+{
+    protected SteamVR_TrackedController controller;
+    public GameObject projectionScreen;
+    protected VideoPlayer Video;
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        Video = projectionScreen.gameObject.GetComponent<VideoPlayer>();   
+    }
+    
+    public void VideoButton()
+    {
+        //Video.enable();
+        Video.Play();
+    }
+    /*
+    void jpgButton()
+    {
+        projectionScreen.GetComponent<>()
+    }*/
+}
