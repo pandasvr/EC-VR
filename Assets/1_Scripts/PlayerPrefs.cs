120d95ac3554650e2503188fc1a1737fdac6170f
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-06-06 16:01:10 +0200

    début de l'impémentation de la condition pour le statut "absent"

--- Assets/1_Scripts/PlayerPrefs.cs
+++ Assets/1_Scripts/PlayerPrefs.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Timers;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerPrefs : MonoBehaviour
 {
@@ -18,6 +22,8 @@ public class PlayerPrefs : MonoBehaviour
         UnityEngine.PlayerPrefs.SetString("labelUserLevel", labelUserLevel);
         UnityEngine.PlayerPrefs.SetString("userFirstName", userFirstName);
         UnityEngine.PlayerPrefs.SetString("userLastName", userLastName);
+        
+        UnityEngine.PlayerPrefs.SetString("status", "connecté");
     }
 
     public void Disconnect()
@@ -40,5 +46,72 @@ public class PlayerPrefs : MonoBehaviour
         }
 
         return rt;
+    }
+
+    
+
+    protected bool userMoves(Vector3 PreviousPosition, Vector3 Newposition)
+    {
+        bool userIsMoving = true;
+        if (false)
+        {
+            userIsMoving = false;
+        }
+        return userIsMoving;
+    }
+    
+    //fonction qui change la valeur du statut de l'utilisateur
+    protected string updateStatus(float timer, Vector3 previousPosition, Vector3 newPosition)
+    {
+        //le string auquel nous allons attribuer unee valeur selon certaines conditions
+        string status;
+        
+        if (userMoves(PreviousPosition, Newposition))
+        {
+            //if player in menu if player not in menu
+            timer = 0;
+            status = "connecté";
+        }
+        
+        //Tout d'abord, la condition pour savoir si l'utilisateur est absent
+        //condition sur un timer
+        if (timer == 120)
+        {
+            status = "absent";
+        }
+
+        //Ensuite, condition pour savoir si l'utilisateur est dans son menu personnel
+        if (false)
+        {
+            status = "Dans le Menu";
+        }
+        
+        //Sinon, l'utilisateur est présent dans le salon, donc connecté
+        else
+        {
+            status = "connecté";
+        }
+        Debug.Log(status);
+        return status;
+    }
+    
+    
+    
+    protected Vector3 PreviousPosition;
+    protected Vector3 Newposition;
+    protected float timer;
+    protected string status;
+    private void Start()
+    {
+        //previousposition=
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        
+     
+        //à chaque frame, on regarde si le statut a changé
+        updateStatus(timer, PreviousPosition,Newposition);
     }
 }
