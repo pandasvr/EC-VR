5086cf8fff7814eeb6e24f9c33793f2f64e328a9
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-06-07 17:00:03 +0200

    Radial Menu Animation
    modification du visuel des Radial Menu
    Ajout d'animations sur les boutons

---
+++ Assets/1_Scripts/UI/RadialMenuSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialMenuSettings : MonoBehaviour
+{
+    public Color hoverEnterIconColor;
+    public Color hoverExitIconColor;
+
+    public void HoverEnterColorIcon(Image icon)
+    {
+        icon.color = new Color();
+        icon.color = hoverEnterIconColor;
+    }
+    
+    public void HoverExitColorIcon(Image icon)
+    {
+        icon.color = new Color();
+        icon.color = hoverExitIconColor;
+    }
+
+    public void HoverEnterAnimationArc(Animator anim)
+    {
+        anim.SetInteger("isEnter", 1);
+    }
+    
+    public void HoverExitAnimationArc(Animator anim)
+    {
+        anim.SetInteger("isEnter", 0); 
+    }
+}
