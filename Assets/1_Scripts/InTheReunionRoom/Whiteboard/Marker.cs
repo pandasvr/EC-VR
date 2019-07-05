f304ba2e0136e4e1824cdd169327b4ceb3539fa0
Unknown <renaud.goislot@capgemini.com>
2019-07-05 14:55:09 +0200

    Script V.1 du whiteboard

---
+++ Assets/1_Scripts/InTheReunionRoom/Whiteboard/Marker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+    [SerializeField]
+    private Color color;
+
+    [SerializeField]
+    private MeshRenderer[] colouredParts;
+
+    [SerializeField]
+    private Painter painter;
+
+    [SerializeField]
+    private PaintReceiver paintReceiver;
+
+    public void Awake()
+    {
+        foreach (MeshRenderer renderer in colouredParts)
+        {
+            renderer.material.color = color;
+        }
+
+        painter.Initialize(paintReceiver);
+        painter.ChangeColour(color);
+    }
+}
