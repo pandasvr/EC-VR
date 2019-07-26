7971287b0c793538348b0086050b0717350e43eb
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-07-26 14:37:35 +0200

    Modif class FreezeMarker

--- Assets/1_Scripts/InTheReunionRoom/Whiteboard/FreezeMarker.cs
+++ Assets/1_Scripts/InTheReunionRoom/Whiteboard/FreezeMarker.cs
@@ -3,7 +3,7 @@
     using UnityEngine;
     using VRTK;
 
-    public class FreezeMarker
+    public class FreezeMarker : MonoBehaviour
     {
         private GameObject whiteboard;
         private GameObject marker;
