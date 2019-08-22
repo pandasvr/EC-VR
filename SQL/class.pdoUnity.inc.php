0300b016ef2d3b90ac9e5727be464a994a6d2c94
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-08-22 15:08:16 +0200

    Changement adresse IP pour 192.168.0.100

--- SQL/class.pdoUnity.inc.php
+++ SQL/class.pdoUnity.inc.php
@@ -1,12 +1,12 @@
 ﻿<?php
 class PdoUnity
 {   		
-      	private static $server='mysql:host=192.168.0.101';
+      	private static $server='mysql:host=192.168.0.100';
       	private static $bdd='dbname=ecvr_db';
       	private static $user='ecvr_db';
       	private static $pw='!CapgeminiPandas4';
-      	/*private static $user='root';
-      	private static $pw='';*/
+      	//private static $user='root';
+      	//private static $pw='';
 		private static $myPdo; //PHP Base de Données
 		private static $myPdoUnity = null;
 /**
